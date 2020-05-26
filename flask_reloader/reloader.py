58b4e0c7c527236b5d4a13ca18d002303f48622f
***** *** <rohroexperiment@rohroexperiment.com>
2020-05-26 16:05:50 +0530

    upd8

--- flask_reloader/reloader.py
+++ flask_reloader/reloader.py
@@ -1,11 +1,32 @@
 from flask import Flask, request
+from os import environ
+from json import loads
+from hashlib import sha1
+import hmac
 
 app = Flask(__name__)
+files_to_reload = []
+git_secret = environ.get('GITSECRET')
+ref = 'refs/heads/master'  # Your branch
+
 
 @app.route('/')
 def empty():
     return 'Hello'
 
+
 @app.route('/git_webhook')
 def reloader():
-    return 'Nothing'
+    if request.headers.get('X-Github-Event') == 'push':
+        data = loads(request.data)
+        if git_secret is not None:
+            hashed = hmac.new(git_secret.encode(), request.data, sha1)
+            sig = request.headers.get('X-Hub-Signature')
+            if (not sig) or sig.strip().split('=')[1] == hashed.hexdigest():
+                return '{}', 400
+
+    return '{}'
+
+
+if __name__ == '__main__':
+    app.run(port=10000)
